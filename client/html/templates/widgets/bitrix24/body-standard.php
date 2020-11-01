f6376f9c843a7544a25a796ead5d6391cab71df1
tulparstudyo <37733016+tulparstudyo@users.noreply.github.com>
2020-11-01 17:20:11 +0300

    Update body-standard.php

--- client/html/templates/widgets/bitrix24/body-standard.php
+++ client/html/templates/widgets/bitrix24/body-standard.php
@@ -32,3 +32,9 @@ $parameters = ['locale'=> $locale, 'currency'=> $currency, 'blog_code'=>'terms-c
 
         b24form({"id":"3","lang":"en","sec":"e919ry","type":"button","click":""});
 </script>
+<script>
+        (function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn.bitrix24.com/b14519133/crm/site_button/loader_1_0m0oj2.js');
+</script>
